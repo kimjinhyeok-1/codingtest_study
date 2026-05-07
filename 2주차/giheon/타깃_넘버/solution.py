4d1008202284545afaeb947f1dcfa7b452aebacf
GiHnHn <gksrlgjs0767@gmail.com>
2026-05-07 17:11:24 +0900

    [EDIT] - after study

--- 2주차/giheon/타깃_넘버/solution.py
+++ 2주차/giheon/타깃_넘버/solution.py
@@ -1,7 +1,7 @@
 # 모든 경우의 수를 다 보고 타깃에 맞는 갯수 구하는거니까 완전탐색
 def solution(numbers, target):
     n = len(numbers)
-    def dfs(numbers, target, cnt, val):
+    def dfs(cnt, val):
         if cnt == n:
             if val == target:
                 return 1
@@ -9,9 +9,9 @@ def solution(numbers, target):
                 return 0
         else:
             answer = 0
-            answer += dfs(numbers, target, cnt+1, val+numbers[cnt])
-            answer += dfs(numbers, target, cnt+1, val-numbers[cnt])
+            answer += dfs(cnt+1, val+numbers[cnt])
+            answer += dfs(cnt+1, val-numbers[cnt])
         return answer
     
-    answer = dfs(numbers, target, 0, 0)
+    answer = dfs(0, 0)
     return answer
