81845504dbb1a5219fc0dc95513a7dee7bf4e632
김진혁 <kjh@gimjinhyeog-ui-MacBookPro.local>
2026-05-10 16:01:53 +0900

    3주차: H-index 완료

--- 3주차/jinhyeok/H-index/solution.py
+++ 3주차/jinhyeok/H-index/solution.py
@@ -1,12 +1,28 @@
-from collections import defaultdict
+# 1번 풀이
+from bisect import bisect_left
 
-def solution(clothes):
-    ans = 1
-    dic = defaultdict(int)
-    for name, kind in clothes:
-        dic[kind] += 1
+def solution(cit):
+    answer = 0
+    cit.sort()
+    l = len(cit)
     
-    for v in dic.values():
-        ans *= (v+1)
+    print(cit)
+    for i in range(l):
+        idx = l - bisect_left(cit, cit[i])
+        if idx >= cit[i]:
+            return cit[i]
+    return answer
+
+# 2번 풀이
+def solution(cit):
+    answer = 0
+    cit.sort()
+    l = len(cit)
     
-    return ans - 1
+    for i in range(l):
+        # i번째 논문 인용 횟수 이상인 것들은 l - i
+        h = l - i
+        # 만약 i번재 논문 인용 횟수가 h 이상이면 바로 return
+        if cit[i] >= h:
+            return h
+    return answer
